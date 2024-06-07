c57279abf51e215a469c3db3ad2218a7c4bd79c0
dev6soft <team.dev6soft@gmail.com>
2024-06-07 22:01:40 +0000

    Criação de um ficheiro de classes e classes de testes para Paralelepipedo.

---
+++ classes/Paralelepipedo.java
@@ -0,0 +1,64 @@
+package classes;
+
+public class Paralelepipedo extends FigurasNaoPlanas implements Cloneable {
+
+    private double comprimento;
+    private double largura;
+    private double altura;
+
+    public Paralelepipedo() {
+        super.setNome("Paralelepipedo");
+    }
+
+    public Paralelepipedo(double comprimento, double largura, double altura) {
+        super.setNome("Paralelepipedo");
+        this.comprimento = comprimento;
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double getComprimento() {
+        return comprimento;
+    }
+    public void setComprimento(double comprimento) {
+        this.comprimento = comprimento;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Paralelepipedo{" +
+                "comprimento='" + getComprimento() + "'" +
+                ", largura='" + getLargura() + "'" +
+                ", altura='" + getAltura() + "'" +
+                '}';
+    }
+
+    @Override
+    public double calcularArea() {
+        return 2 * (comprimento * largura + comprimento * altura + largura * altura);
+    }
+
+    @Override
+    public double calcularVolume() {
+        return comprimento * largura * altura;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+}
---
+++ tests/ParalelepipedoTest.java
@@ -0,0 +1,81 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import classes.Paralelepipedo;
+
+public class ParalelepipedoTest {
+
+    // Declaração das variáveis utilizadas nos testes
+    private Paralelepipedo paralelepipedo;
+    private double comprimento = 5.0;
+    private double largura = 7.0;
+    private double altura = 10.0;
+     
+    // Esta função é executada antes de cada teste.
+    @BeforeEach
+    public void setup() {
+       paralelepipedo = new Paralelepipedo(comprimento, largura, altura);
+    }
+
+    @Test
+    public void testGetComprimento() {
+       Assertions.assertEquals(comprimento, paralelepipedo.getComprimento());
+    }
+
+    @Test
+    public void testSetComprimento() {
+       paralelepipedo.setComprimento(comprimento);
+       Assertions.assertEquals(comprimento, paralelepipedo.getComprimento());
+    }
+
+    @Test
+    public void testGetLargura() {
+       Assertions.assertEquals(largura, paralelepipedo.getLargura());
+    }
+
+    @Test
+    public void testSetLargura() {
+       paralelepipedo.setLargura(largura);
+       Assertions.assertEquals(largura, paralelepipedo.getLargura());
+    }
+
+    @Test
+    public void testGetAltura() {
+        Assertions.assertEquals(altura, paralelepipedo.getAltura());
+    }
+
+    @Test
+    public void testSetAltura() {
+        paralelepipedo.setAltura(altura);
+        Assertions.assertEquals(altura, paralelepipedo.getAltura());
+    }
+
+    @Test
+    public void testToString() {
+        String stringEsperada = "Paralelepipedo{comprimento='5.0', largura='7.0', altura='10.0'}";
+        Assertions.assertEquals(stringEsperada, paralelepipedo.toString());
+    }
+
+    @Test
+    public void testCalcularArea() {
+        double areaEsperada = 2 * (comprimento * largura + comprimento * altura + largura * altura);
+        Assertions.assertEquals(areaEsperada, paralelepipedo.calcularArea());
+    }
+
+    @Test
+    public void testCalcularVolume() {
+        double volumeEsperado = comprimento * largura * altura;
+        Assertions.assertEquals(volumeEsperado, paralelepipedo.calcularVolume());
+    }
+
+    public void testClone() throws CloneNotSupportedException {
+        Paralelepipedo paralelepipedoClone = (Paralelepipedo) paralelepipedo.clone();
+
+        Assertions.assertEquals(paralelepipedo.getComprimento(), paralelepipedoClone.getComprimento());
+        Assertions.assertEquals(paralelepipedo.getLargura(), paralelepipedoClone.getLargura());
+        Assertions.assertEquals(paralelepipedo.getAltura(), paralelepipedoClone.getAltura());
+
+        Assertions.assertNotSame(paralelepipedo, paralelepipedoClone);
+    }
+
+}
