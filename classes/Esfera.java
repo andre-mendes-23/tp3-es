e907580cd0bd6e288826bb952e22b73cb238aea0
dev6soft <team.dev6soft@gmail.com>
2024-06-07 22:40:46 +0000

    Criação de um ficheiro de classes e classes de testes para Esfera.

---
+++ classes/Esfera.java
@@ -0,0 +1,51 @@
+package classes;
+
+public class Esfera extends FigurasNaoPlanas implements Cloneable {
+
+    public double raio;
+    final double pi = 3.14;
+    
+    public Esfera() {
+        super.setNome("Esfera");
+    }
+
+    public Esfera(double raio) {
+        super.setNome("Esfera");
+        this.raio = raio;
+    }
+    
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    
+    public double getPi() {
+        return pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Esfera{" +
+                "raio='" + getRaio() + "'" +
+                '}';
+    }
+
+    @Override
+    public double calcularArea() {
+        return 4.0 * pi * raio * raio;
+    }
+
+    @Override
+    public double calcularVolume() {
+        return (4.0 / 3.0) * pi * raio * raio * raio;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+
+    }
+
+}
---
+++ tests/EsferaTest.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import classes.Esfera;
+
+public class EsferaTest {
+
+    // Declaração das variáveis utilizadas nos testes
+    private Esfera esfera;
+    private double raio = 5.0;
+    final double pi = 3.14;
+
+    // Esta função é executada antes de cada teste
+    @BeforeEach
+    public void setup() {
+        esfera = new Esfera(raio);
+    }
+
+    @Test
+    public void testGetRaio() {
+        Assertions.assertEquals(raio, esfera.getRaio());
+    }
+
+    @Test
+    public void testSetRaio() {
+        esfera.setRaio(raio);
+        Assertions.assertEquals(raio, esfera.getRaio());
+    }
+
+    public void testGetPi() {
+        Assertions.assertEquals(pi, esfera.getPi());
+    }
+
+    @Test
+    public void testToString() {
+        String stringEsperada = "Esfera{raio='5.0'}";
+        Assertions.assertEquals(stringEsperada, esfera.toString());
+    }
+
+    @Test
+    public void testCalcularArea() {
+        double areaEsperada = 4.0 * pi * raio * raio;
+        Assertions.assertEquals(areaEsperada, esfera.calcularArea());
+    }
+
+    @Test
+    public void testCalcularVolume() {
+        double volumeEsperado = (4.0 / 3.0) * pi * raio * raio * raio;
+        Assertions.assertEquals(volumeEsperado, esfera.calcularVolume());
+    }
+
+    @Test
+    public void testClone() throws CloneNotSupportedException {
+        Esfera esferaClone = (Esfera) esfera.clone();
+
+        Assertions.assertEquals(esfera.getRaio(), esferaClone.getRaio());
+
+        Assertions.assertNotSame(esfera, esferaClone);
+
+    }
+
+}
