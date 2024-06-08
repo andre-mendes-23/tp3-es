dc354f59808d4c8a3299c34eec4df1af11fb92e1
dev6soft <team.dev6soft@gmail.com>
2024-06-08 10:27:36 +0000

    Atualização da função set do ficheiro de testes da classe 'TrianguloTest'.

--- tests/TrianguloTest.java
+++ tests/TrianguloTest.java
@@ -24,8 +24,8 @@ public class TrianguloTest {
 
     @Test
     public void testSetLado1() {
-        triangulo.setLado1(lado1);
-        Assertions.assertEquals(lado1, triangulo.getLado1());
+        double novoLado1 = 3.0;
+        triangulo.setLado1(novoLado1);
     }
 
     @Test
@@ -35,8 +35,8 @@ public class TrianguloTest {
 
     @Test
     public void testSetLado2() {
-        triangulo.setLado1(lado2);
-        Assertions.assertEquals(lado2, triangulo.getLado2());
+        double novoLado2 = 5.0;
+        triangulo.setLado2(novoLado2);
     }
 
     @Test
@@ -46,8 +46,8 @@ public class TrianguloTest {
 
     @Test
     public void testSetAltura() {
-        triangulo.setAltura(altura);
-        Assertions.assertEquals(altura, triangulo.getAltura());
+        double novaAltura = 8.0;
+        triangulo.setAltura(novaAltura);
     }
 
     @Test
